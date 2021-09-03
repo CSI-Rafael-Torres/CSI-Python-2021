e161bbf3ac4cca68f23657366d100c6016430bbc
CSI-Rafael-Torres <22-081@sanignacio.pr>
2021-09-03 10:11:44 -0400

    dynamic array

---
+++ Modules/Module3/array.py
@@ -0,0 +1,20 @@
+s=1
+arr = []
+print("put array size ")
+n = int(input())
+print("put the array",s,"value ")
+x = int(input())
+arr.append(x)
+for s in range(0,n-1):
+    if s<n:
+        s += 1
+        print("put the array",s+1, "value")
+        x = int(input())
+        arr.append(x)
+print(arr)
+arrs = [4, 3, -1, 4, 34]
+
+
+    
+    
+
