0720e5fb0e2901f6907181ab930603fc08d5f215
CSI-Rafael-Torres <22-081@sanignacio.pr>
2021-10-28 09:24:39 -0400

    Create Rafael-Torres-Projectile-Motion.py

---
+++ Projects/Projectile-Motion/Rafael-Torres-Projectile-Motion.py
@@ -0,0 +1,11 @@
+import math
+time =0
+Gun = "APB"
+AW=0.011#kg
+PS=302#m/s
+Caliber= "9x18mm Makarov"
+Ammo= "9x18mm PM P gzh"
+Building= "Ocean Tower"
+Height =242.92#ft
+print("An",Gun,"with",Ammo,"ammo and",Caliber,"caliber was fired out of",Building,"with a velocity of",PS,"This program with find its time and distance")
+d= PS*time
