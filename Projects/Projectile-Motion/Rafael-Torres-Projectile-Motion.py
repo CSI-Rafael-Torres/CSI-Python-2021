c26194226affcc5c7009a10c689e7e9eefefb3cc
CSI-Rafael-Torres <22-081@sanignacio.pr>
2021-11-05 08:41:47 -0400

    update

--- Projects/Projectile-Motion/Rafael-Torres-Projectile-Motion.py
+++ Projects/Projectile-Motion/Rafael-Torres-Projectile-Motion.py
@@ -1,11 +1,16 @@
 import math
-time =0
-Gun = "APB"
-AW=0.011#kg
-PS=302#m/s
-Caliber= "9x18mm Makarov"
-Ammo= "9x18mm PM P gzh"
-Building= "Ocean Tower"
-Height =242.92#ft
-print("An",Gun,"with",Ammo,"ammo and",Caliber,"caliber was fired out of",Building,"with a velocity of",PS,"This program with find its time and distance")
-d= PS*time
+# time =0
+# gravity =9.81
+# Gun = "APB"
+# AW=0.011#kg
+# PS=302#m/s
+# Caliber= "9x18mm Makarov"
+# Ammo= "9x18mm PM P gzh"
+# Building= "Ocean Tower"
+# Height =242.92#ft
+# d= PS*time
+def GProjectile(Gun:str, Caliber:str, Ammo:str,Building:str,Height:int,Speed:int,Gravity:int):
+    Time = math.sqrt((2*Height)/Gravity)
+    distance=(Speed*Time)
+    print("An",Gun,"with",Ammo,"ammo and",Caliber,"caliber was fired out of",Building,"with a velocity of",Speed,"This program with find its time and distance")
+GProjectile("APB","9x18mm Makarov","9x18mm PM P gzh","Ocean Tower",242.92,302,9.81)
