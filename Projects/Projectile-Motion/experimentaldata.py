b69f8a3191d9c7c19f48d51d5e1d2c36638fe794
CSI-Rafael-Torres <22-081@sanignacio.pr>
2021-11-15 09:15:09 -0400

    proj

--- Projects/Projectile-Motion/experimentaldata.py
+++ Projects/Projectile-Motion/experimentaldata.py
@@ -1,9 +1,10 @@
-class experimentaldata:
-    def __innit__(self,Gun:str, Caliber:str, Ammo:str,Building:str,Height:int,Speed:int,Gravity:int):
+class Experimentaldata:
+    def __innit__(self,Gun:str, Caliber:str, Ammo:str,Building:str,Height:int,Speed:int,Planet:str):
         self.Gun=Gun
         self.Caliber=Caliber
         self.Ammo=Ammo
         self.Building = Building
         self.Height=Height
         self.Speed=Speed
-        self.Gravity=Gravity
+        # self.Gravity=Gravity
+        self.Planet=Planet
